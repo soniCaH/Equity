ee635f4a4cd0d5cbd844fd1f7b274d0ce5cf3655
Kevin Van Ransbeeck <climacon@gmail.com>
2012-01-18 14:57:33 +0100

    Added Icon
    Added Eq icon

--- src/be/khleuven/vanransbeeck/kevin/EquityHelpActivity.java
+++ src/be/khleuven/vanransbeeck/kevin/EquityHelpActivity.java
@@ -3,13 +3,9 @@ package be.khleuven.vanransbeeck.kevin;
 import android.os.Bundle;
 
 public class EquityHelpActivity extends EquityActivity {
-	
-<<<<<<< HEAD
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.help);
 	}
-=======
->>>>>>> Unsynched
 }
