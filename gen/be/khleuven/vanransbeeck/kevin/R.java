967cad549dcfe4c25f8e5d566f3447173d573832
Kevin Van Ransbeeck <climacon@gmail.com>
2012-01-20 12:27:58 +0100

    Translation Test
    Translation Test (Bing API and wrapper)

--- gen/be/khleuven/vanransbeeck/kevin/R.java
+++ gen/be/khleuven/vanransbeeck/kevin/R.java
@@ -39,14 +39,15 @@ public final class R {
         public static final int start=0x7f020009;
     }
     public static final class id {
-        public static final int ListView_Menu=0x7f090005;
+        public static final int ListView_Menu=0x7f090006;
         public static final int RelativeLayout1=0x7f090000;
         public static final int imageView1=0x7f090003;
-        public static final int menu_icon=0x7f090006;
-        public static final int menu_label=0x7f090007;
+        public static final int menu_icon=0x7f090007;
+        public static final int menu_label=0x7f090008;
         public static final int scrollView1=0x7f090002;
         public static final int textView1=0x7f090001;
-        public static final int textView2=0x7f090008;
+        public static final int textView2=0x7f090009;
+        public static final int textView7=0x7f090005;
         public static final int webView1=0x7f090004;
     }
     public static final class layout {
--- src/be/khleuven/vanransbeeck/kevin/EquityLanguageActivity.java
+++ src/be/khleuven/vanransbeeck/kevin/EquityLanguageActivity.java
@@ -1,11 +1,34 @@
 package be.khleuven.vanransbeeck.kevin;
 
+import java.util.Map;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.memetix.mst.language.Language;
+import com.memetix.mst.translate.Translate;
+import com.memetix.mst.detect.Detect;
+
 
 public class EquityLanguageActivity extends EquityActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.language);
+	    TextView textView = (TextView) findViewById(R.id.textView7);
+		
+	    Translate.setKey("F201136C4151E60657AFB717096F87C70F45EF30");
+	    try {
+//			String translatedText = Translate.execute("Bonjour le monde", Language.FRENCH, Language.ENGLISH);
+//			textView.setText(translatedText);
+	    	Map<String,Language> localizedMap = Language.values(Language.FRENCH);
+	    	for(String langName : localizedMap.keySet()) {
+	            Log.i("Equity", langName + " : " + localizedMap.get(langName).toString());
+	        }
+		} catch (Exception e) {
+			textView.setText("OEPS");
+		}
+	    
 	}
 }
